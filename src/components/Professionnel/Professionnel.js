93b5d37ccd7345d4223e7fdddcae25a8028321e6
believelody <believelody@gmail.com>
2018-11-15 12:48:07 +0100

    Update professionel component

--- src/components/Professionnel/Professionnel.js
+++ src/components/Professionnel/Professionnel.js
@@ -2,6 +2,31 @@ import React from 'react';
 
 const boxes = [
   {
+    date: "2018 - En Cours",
+    description: `King Djay est un animateur localisé en Normandie. Pour dynamiser son activité hors de sa
+    région, il m’a été demandé de développer un site web vitrine pouvant présenter un bon
+    nombre de ses activités. Aussi, cela fait plus sérieux. Je développe ce site web avec React couplé à Redux. Il est hébergé sur Netlify. Étant donné qu'il y aura une gestion de contenu, il était intéressant de permettre cette gestion sans une intervention de ma part. J'ai alors utilisé un CMS nommé Contentful. Son module Javascript permet de faire très simplement des requêtes http afin de récupérer les données à afficher. Ainsi, King Djay n'a juste qu'à mettre son contenu sur Contentful, le site web récupérera tout cela et adaptera sa vue. Une dernière chose, étant aussi DJ, j'ai eu la bonne idée d'inclure un lecteur audio afin de promouvoir ses mixs que j'ai développé from scratch.`,
+    link: "https://kingdjayk.netlify.com",
+    title: "Site web vitrine pour un animateur professionnel"
+  },
+  {
+    date: "2018 - En Cours",
+    description: `Le projet ITM est une mise en situation réelle d’interconnexion entre terminaux mobiles. Le
+    sujet proposé est de mettre à disposition d’une entreprise des systèmes pouvant intéragir
+    entre eux. Ceci est un travail de groupe. Je m’occupe de la conception d’une application web
+    qui permettrait à la RH de gérer les présences et absences du personnel et voir en temps réel
+    leur position dans le batiment. Je développe cette application avec React et j'utilise firebase comme base de données. 3 autres applications mobiles ont été développées sous Android Studio. Nous avons utilisé les technologies bluetooth et NFC. Le projet est toujours en cours.`,
+    link: "https://itmproject.netlify.com",
+    title: "Projet Universitaire ITM"
+  },
+  {
+    date: "2017 - 2018",
+    description: `Ce projet a été réalisé avec le framework hybride Ionic pour la partie mobile et Angular pour le site web.
+    FlashLike est une application destinée à améliorer l'e-reputation d'un commerce, association ou institution désirant avoir une visibilité sur les réseaux sociaux. Subdivisé en 2 parties, une partie application et une partie site web, elle doit permettre d'un côté, aux commerçants désignés par le terme "adhérent" de pouvoir enregistrer leur local via leur coordonnées GPS et de mettre à disposition des clients finaux appelés "Flashliker" un code QR à flasher, et de l'autre une application mobile permettant aux Flashlikers de scanner le QR Code d'un adhérent puis liker ou disliker le lieu. La notion principale vue lors de ce projet est la synchronisation de deux applications distinctes. En effet, il fallait que les changements opérés dans l'une se répercutent dans l'autre. Pour se faire, j'ai utilisé la plateforme cloud firebase et son plugin dédié à Angular nommé AngularFire.`,
+    link: "https://flashlike-8d9ed.firebaseapp.com/",
+    title: "Projet universitaire Ionic: FlashLike"
+  },
+  {
     date: "2017 - 2018",
     description: `Ce projet est une compilation de tout mon apprentissage javascript.
     En tant qu'autodidacte, j'ai commencé une série de mini-projets sur divers concepts, problématiques, notions pertinents régulièrement rencontrés dans le web development. Je me focalise particulièrement sur le css et le javascript. Pour celà ces mini-projets sont faits en pur html - css - vanilla. De plus, c'est aussi une occasion pour moi de développer les nombreuses possibilités qu'offre l'ES6, mettre en pratique les notions de bases du développement web et explorer le grand panel d'outils et API qui accompagne javascript. J'aborde les notions suivantes:\r\n
@@ -27,13 +52,6 @@ const boxes = [
     Concernant firebase, c'est une plateforme cloud construite par Google proposant l'authentification, la base, l'hébergement et bien d'autres services. Sa base de données est définie comme NoSQL. Légère, rapide et simple d'utilisation, il faut cependant se familiariser avec sa documentation. La notion principale mise en évidence ici est la programmation réactive, concept qui m'intéresse énormément.`,
     link: "https://react-app-firebase-post.herokuapp.com/",
     title: "Projet personnel React Firebase Post"
-  },
-  {
-    date: "2014 - 2015",
-    description: `Ce projet a été réalisé avec le framework hybride Ionic pour la partie mobile et AngularJS pour le site web.
-    FlashLike est une application destinée à améliorer l'e-reputation d'un commerce, association ou institution désirant avoir une visibilité sur les réseaux sociaux. Subdivisé en 2 parties, une partie application et une partie site web, elle doit permettre d'un côté, aux commerçants désignés par le terme "adhérent" de pouvoir enregistrer leur local via leur coordonnées GPS et de mettre à disposition des clients finaux appelés "Flashliker" un code QR à flasher, et de l'autre une application mobile permettant aux Flashlikers de scanner le QR Code d'un adhérent puis liker ou disliker le lieu. A ce jour, le projet a été refait avec Ionic 2 et Angular 2. La notion principale vue lors de ce projet est la synchronisation de deux applications distinctes. En effet, il fallait que les changements opérés dans l'une se répercutent dans l'autre. Pour se faire, j'ai utilisé la plateforme cloud firebase et son plugin dédié à Angular nommé AngularFire.`,
-    link: "https://flashlike-8d9ed.firebaseapp.com/",
-    title: "Projet universitaire Ionic: FlashLike"
   },
   {
     date: "2013 - 2014",
